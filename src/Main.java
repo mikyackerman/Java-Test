648f3ce88d7e2ed517a6205673757bb50df0bcc8
Jorge Atempa <atempaitm@gmail.com>
2017-01-29 00:43:43 -0800

    Probar  la clase Persona

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import com.github.javafaker.Faker;
+import model.Persona;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Faker faker = new Faker();
+
+        Persona persona = new Persona();
+        persona.setNombre(faker.name().firstName());
+        persona.setApellidoPaterno(faker.name().lastName());
+        persona.setApellidoMaterno(faker.name().lastName());
+        persona.setEdad(faker.number().numberBetween(18,60));
+        persona.setDireccion(faker.address().streetAddress());
+
+        System.out.println(persona);
+
     }
 }
