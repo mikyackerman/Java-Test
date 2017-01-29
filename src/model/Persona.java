bb6ca325db35695b0e795ee27ec70ca4ebfdd184
Jorge Atempa <atempaitm@gmail.com>
2017-01-29 00:14:20 -0800

    Crear clase Persona

---
+++ src/model/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+     private int id_persona;
+     private String nombre;
+     private String apellido_paterno;
+     private String apellido_materno;
+     private int edad;
+     private String direccion;
+
+     public Persona() {
+     }
+}
