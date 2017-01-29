c7ac05304c6310b57ee343eb4117c8e6409016f5
Jorge Atempa <atempaitm@gmail.com>
2017-01-29 01:06:59 -0800

    Establecer valores estáticos

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import com.github.javafaker.Faker;
 import model.Persona;
 
 public class Main {
 
     public static void main(String[] args) {
-        Faker faker = new Faker();
-
         Persona persona = new Persona();
-        persona.setNombre(faker.name().firstName());
-        persona.setApellidoPaterno(faker.name().lastName());
-        persona.setApellidoMaterno(faker.name().lastName());
-        persona.setEdad(faker.number().numberBetween(18,60));
-        persona.setDireccion(faker.address().streetAddress());
+        persona.setIdPersona(1);
+        persona.setNombre("Jorge");
+        persona.setApellidoPaterno("Atempa");
+        persona.setApellidoMaterno("Camacho");
+        persona.setEdad(29);
+        persona.setDireccion("Av. Siempre Viva");
 
         System.out.println(persona);
 
